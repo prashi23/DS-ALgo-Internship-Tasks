17f17de1c137057606e4e790ae6efcea8d91a0cd
Prashi Doval <prashidoval23@gmail.com>
2020-01-01 05:08:52 +0530

    Update first.cpp

--- Task1/Stacks/First Approach/first.cpp
+++ Task1/Stacks/First Approach/first.cpp
@@ -1,1 +1,47 @@
+#include <bits/stdc++.h> 
+using namespace std;
+class Stack 
+{  
+    queue<int> q1, q2;
+    public: 
+    void push(int x) 
+    { 
+        q2.push(x); 
+        while (!q1.empty()) 
+        { 
+            q2.push(q1.front()); 
+            q1.pop(); 
+        }
+        queue<int> q = q1; 
+        q1 = q2; 
+        q2 = q; 
+    } 
+    void pop()
+    { 
+        if (q1.empty())  
+			      return ; 
+        q1.pop(); 
+    } 
+    int top() 
+    { 
+        if (q1.empty())  
+			      return -1; 
+        return q1.front(); 
+    } 
+ }; 
+int main() 
+{ 
+    Stack s; 
+    s.push(1); 
+    s.push(10); 
+    s.push(5);
+    s.push(3); 
+    s.push(7);
+    cout<<s.top()<<"\n"; 
+    s.pop(); 
+    cout <<s.top()<<"\n"; 
+    s.pop(); 
+    cout <<s.top()<<"\n"; 
+    return 0; 
+} 
 
