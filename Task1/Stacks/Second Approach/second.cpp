4eb8d6ca0165769beaec98f7750020e91cb3456d
Prashi Doval <prashidoval23@gmail.com>
2020-01-01 05:39:16 +0530

    Update second.cpp

--- Task1/Stacks/Second Approach/second.cpp
+++ Task1/Stacks/Second Approach/second.cpp
@@ -1,1 +1,44 @@
+#include<bits/stdc++.h> 
+using namespace std;
+class Stack 
+{ 
+    queue<int> q1, q2;
+    public:
+    void push(int x) {
+        q1.push(x);
+    }
+    
+    int pop() {
+        int res;
+        while(!q1.empty())
+        {
+            if(q1.size() == 1)
+                res = q1.front();
+            else
+                q2.push(q1.front());
+            q1.pop();
+        }
+        q2.swap(q1);
+        return res;
+    }
+    
+    int top() {
+        
+        int res;
+        while(!q1.empty())
+        {
+            if(q1.size() == 1)
+                res = q1.front();
+            
+            q2.push(q1.front());
+            q1.pop();
+        }  
+        
+        q2.swap(q1);
+        return res;
+    }
 
+    bool empty() {
+        return (q1.empty());
+    }
+};
