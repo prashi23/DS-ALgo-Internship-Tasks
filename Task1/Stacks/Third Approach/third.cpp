807fcf9c256a58ef4823bc1a4d641b92f7b29065
Prashi Doval <prashidoval23@gmail.com>
2020-01-01 06:53:32 +0530

    Update third.cpp

--- Task1/Stacks/Third Approach/third.cpp
+++ Task1/Stacks/Third Approach/third.cpp
@@ -1,1 +1,34 @@
+include<bits/stdc++.h> 
+using namespace std;
+class Stack 
+{ 
+int n;
 
+queue<int>q1;
+public:
+void push(int x) {
+    q1.push(x);
+    n++;
+    for (int i = 0; i < q1.size()-1; i++)
+    {
+        q1.push(q1.front());
+        q1.pop();
+    }
+    
+}
+
+int pop() {
+    n--;
+    int a = q1.front();
+    q1.pop();
+    return a;
+    
+}
+int top() {
+    return q1.front();
+    
+}
+bool empty() {
+    return (n==0);
+    
+}};
